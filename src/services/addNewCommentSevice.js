f2d2b2f5fb71dd21e73f487e7c4cb776ee21fe6a
Amirhossein <amirhossinbalagar@gmail.com>
2021-11-11 15:25:48 +0330

    send token in header

--- src/services/addNewCommentSevice.js
+++ src/services/addNewCommentSevice.js
@@ -1,5 +1,11 @@
 import http from "./httpService";
 
-export function addNewComments(data) {
-    return http.post("/comments", data);
+export function addNewComments(data, token) {
+    const token = 'SECURE TOKEN !';
+    const header = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    return http.post("/comments", data, header);
 }
