20bc195a6f29f1e263b6df38aa596730e89598ce
Amirhossein <amirhossinbalagar@gmail.com>
2021-11-11 14:52:14 +0330

    add Axios-interceptor and instance

--- src/services/deleteCommentService.js
+++ src/services/deleteCommentService.js
@@ -1,5 +1,6 @@
-import http from "./httpService";
+import instance from "../axiosInstance";
+//import http from "./httpService";
 
 export function deleteComment(commentId) {
-    return http.delete(`/comments/${commentId}`);
+    return instance.delete(`/comments/${commentId}`);
 }
